7b5161368bb1c26931989351b132c497534eb096
Warren Ulrich <warren.ulrich@outlook.com>
2025-09-26 14:44:16 -0700

    Added BehaviorScript

---
+++ src/main/java/com/warren/bt/BehaviorScript.java
@@ -0,0 +1,21 @@
+package com.warren.bt;
+
+import java.lang.module.ModuleDescriptor.Builder;
+
+import org.powbot.api.script.AbstractScript;
+
+public abstract class BehaviorScript extends AbstractScript {
+  private BehaviorTree behaviorTree;
+  
+  @Override
+  public void onStart() {
+    behaviorTree = builder().build();
+  }
+
+  @Override
+  public final void poll() {
+    behaviorTree.run();
+  }
+  
+  public abstract BehaviorTree.Builder builder();
+}
